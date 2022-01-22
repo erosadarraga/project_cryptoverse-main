643cca87f164b6aa74a2a540399a953a30839215
Eros <eroslbac123456@gmail.com>
2022-01-22 07:28:56 -0500

    cambios

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,12 +1,12 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 // Note: Change v1 to v2 on rapid api
 
 const cryptoApiHeaders = {
   'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
   'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-};
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+}
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
@@ -22,19 +22,17 @@ export const cryptoApi = createApi({
 
     // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
     getCryptoHistory: builder.query({
-      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+      query: ({ coinId, timeperiod }) =>
+        createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
     }),
 
     // Note: To access this endpoint you need premium plan
-    getExchanges: builder.query({
-      query: () => createRequest('/exchanges'),
-    }),
   }),
-});
+})
 
 export const {
   useGetCryptosQuery,
   useGetCryptoDetailsQuery,
   useGetExchangesQuery,
   useGetCryptoHistoryQuery,
-} = cryptoApi;
+} = cryptoApi
